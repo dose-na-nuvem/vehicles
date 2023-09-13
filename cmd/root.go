e6bb8efe167f0bcd58614e5b5fa2cdeddbb70c95
EzzioMoreira <ezzio.moreira@pagar.me>
2023-09-13 07:54:24 -0300

    feat: adiciona arquivo de configuracao viper

--- cmd/root.go
+++ cmd/root.go
@@ -4,9 +4,19 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/dose-na-nuvem/customers/config"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
-// rootCmd represents the base command when called without any subcommands
+// Configura o nome padrão do arquivo de configuração.
+var (
+	configFile     string
+	cfg            = config.New()
+	defaultTimeout = 5 * time.Second
+)
+
+// rootCmd representa o comando base quando chamado sem nenhum subcomando.
 var rootCmd = &cobra.Command{
 	Use: "vehicles",
 
@@ -16,11 +26,41 @@ var rootCmd = &cobra.Command{
 Será possível adicionar carros com multiplas tags e relacionar os carros com os donos atuais.`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+// Execute adiciona todos os comandos filhos ao comando raiz e configura as bandeiras apropriadamente.
+// Isso é chamado por main.main(). Isso só precisa acontecer uma vez para o rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
+	}
+}
+
+func initConfig() {
+	// Configura o nome padrão do arquivo de configuração, sem a extensão.
+	viper.SetConfigFile(configFile)
+
+	// Tenta ler o arquivo de configuração, ignorando erros caso o mesmo não seja encontrado
+	// retorna um erro se não conseguirmos analisar o arquivo de configuração encontrado.
+	if err := viper.ReadInConfig(); err != nil {
+		// Não há problems se não existir um arquivo de configuração.
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			cfg.Logger.Error("arquivo não encontrado",
+				zap.String("arquivo", configFile),
+				zap.Error(err),
+			)
+
+			return
+		}
+
+		cfg.Logger.Error("falha na leitura do arquivo de configuração", zap.Error(err))
+	} else {
+		cfg.Logger.Info("arquivo de configuração lido", zap.String("config", configFile))
+	}
+
+	// Converter o estado interno do Viper em nosso objeto de configuração
+	if err := viper.Unmarshal(&cfg); err != nil {
+		cfg.Logger.Error("falhou ao converter o arquivo de configuração", zap.Error(err))
+
+		return
 	}
 }
---
+++ config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import "go.uber.org/zap"
+
+type Cfg struct {
+	Database Database       `mapstructure:"db"`
+	Server   ServerSettings `mapstructure:"server"`
+
+	Logger *zap.Logger
+}
+
+type Database struct {
+	Type     string `mapstructure:"type"`
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+}
+
+func New() *Cfg {
+	cfg := &Cfg{
+		Logger: zap.Must(zap.NewDevelopment()),
+	}
+	return cfg
+}
