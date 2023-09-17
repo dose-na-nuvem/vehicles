ff4e3d2f487441fa85003929a2221416bd325676
Claudio B <claudiobastos@gmail.com>
2023-09-17 19:25:51 -0300

    servidor http blocante

---
+++ pkg/server/http.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/dose-na-nuvem/vehicles/config"
+	"go.uber.org/zap"
+)
+
+const (
+	httpStarting = "servidor http iniciando"
+	httpStopping = "servidor http finalizando"
+)
+
+type HTTP struct {
+	logger *zap.Logger
+
+	srv *http.Server
+}
+
+func NewHTTP(cfg *config.Cfg, vehiclesHandler http.Handler) (*HTTP, error) {
+
+	mux := http.NewServeMux()
+	mux.Handle("/", vehiclesHandler)
+
+	srv := &http.Server{
+		Addr:    cfg.Server.HTTP.Endpoint,
+		Handler: mux,
+	}
+
+	return &HTTP{
+		logger: cfg.Logger,
+		srv:    srv,
+	}, nil
+}
+
+func (h *HTTP) Start(ctx context.Context) error {
+	h.logger.Info(httpStarting, zap.String("endpoint", h.srv.Addr))
+	err := h.srv.ListenAndServe()
+
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		h.logger.Error("falha ao iniciar o servidor HTTP", zap.Error(err))
+	}
+
+	return nil
+}
+
+func (h *HTTP) Shutdown(ctx context.Context) error {
+	h.logger.Info(httpStopping)
+	// We received an interrupt signal, shut down.
+	if err := h.srv.Shutdown(ctx); err != nil {
+		// Error from closing listeners, or context timeout:
+		return err
+	}
+
+	return nil
+}
--- pkg/service/vehicles.go
+++ pkg/service/vehicles.go
@@ -2,23 +2,40 @@ package service
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/dose-na-nuvem/vehicles/config"
+	"github.com/dose-na-nuvem/vehicles/pkg/server"
 )
 
 type Vehicle struct {
-	cfg *config.Cfg
+	cfg  *config.Cfg
+	http *server.HTTP
 }
 
 func New(cfg *config.Cfg) *Vehicle {
 	return &Vehicle{
-		cfg: cfg,
+		cfg:  cfg,
+		http: &server.HTTP{},
 	}
 }
 
 func (v *Vehicle) Start(ctx context.Context) error {
+	var err error
+
 	v.cfg.Logger.Info("inicializando...")
 	v.cfg.Logger.Info("🚗💨 vehicles running")
+
+	vh := server.NewVehiclesHandler(v.cfg.Logger)
+	v.http, err = server.NewHTTP(v.cfg, vh)
+	if err != nil {
+		return fmt.Errorf("falha ao configurar o servidor HTTP: %w", err)
+	}
+	err = v.http.Start(ctx)
+	if err != nil {
+		return fmt.Errorf("falha ao iniciar o servidor HTTP: %w", err)
+	}
+
 	return nil
 }
 
