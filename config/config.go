af083949db8e1717196e305c94e58226f28a04e3
Luiz Guilherme Ribeiro <ltrindaderibeiro@gmail.com>
2023-09-20 12:48:00 -0300

    feat: adiciona configurações do servidor grpc + TLS (cobra + viper) (#23)
    Signed-off-by: Luiz Ribeiro <ltrindaderibeiro@gmail.com>

--- config/config.go
+++ config/config.go
@@ -3,12 +3,33 @@ package config
 import "go.uber.org/zap"
 
 type Cfg struct {
+	Server ServerSettings `mapstructure:"server"`
+
 	Logger *zap.Logger
+}
+
+type GRPCServerSettings struct {
+	Endpoint string `mapstructure:"endpoint"`
+}
+
+type TLSSettings struct {
+	CertFile    string `mapstructure:"cert_file"`
+	CertKeyFile string `mapstructure:"cert_key_file"`
+	Insecure    bool   `mapstructure:"insecure"`
+}
+
+
+type ServerSettings struct {
+	GRPC GRPCServerSettings `mapstructure:"grpc"`
+	TLS  *TLSSettings       `mapstructure:"tls"`
 }
 
 func New() *Cfg {
 	cfg := &Cfg{
 		Logger: zap.Must(zap.NewDevelopment()),
+		Server: ServerSettings{
+			TLS: &TLSSettings{},
+		},
 	}
 	return cfg
 }
