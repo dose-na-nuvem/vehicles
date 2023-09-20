6bab0ea5459f9eaab381c1530e3b4c626e93f41b
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2023-09-19 22:45:41 -0300

    feat: adiciona configurações do servidor grpc + TLS (cobra + viper)
    Signed-off-by: Luiz Ribeiro <ltrindaderibeiro@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -33,6 +33,16 @@ func Execute() {
 	rootCmd.PersistentFlags().StringVar(&configFile, "config", "config.yaml",
 		"Define o arquivo de configuração a utilizar.")
 	
+	startCmd.Flags().StringVar(&cfg.Server.TLS.CertFile, "server.tls.certfile", "", "caminho do certificado.")
+
+	startCmd.Flags().StringVar(&cfg.Server.TLS.CertKeyFile, "server.tls.certkeyfile", "",
+		"caminho da chave privada do certificado.")
+
+	startCmd.Flags().BoolVar(&cfg.Server.TLS.Insecure, "server.tls.insecure", false, "Força o modo inseguro.")
+
+	startCmd.Flags().StringVar(&cfg.Server.GRPC.Endpoint, "server.grpc.endpoint", "127.0.0.1:56533",
+		"Endereço gRPC onde o serviço vai servir requisições.")
+
 	// Associa o Viper as flags
 	if err := viper.BindPFlags(startCmd.Flags()); err != nil {
 		cfg.Logger.Error("falha ao ligar as flags", zap.Error(err))
